be3848247a92566c63a7cd9bdfe38ab8360fa3e6
elmyrza1 <elmyrza217@gmail.com>
2024-08-26 19:15:55 +0600

    *

--- super_core/settings.py
+++ super_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5^4c)m4h&0@e5kf%b$atun$n(wyj+0(o94wr-2(1dili0t4vi(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
