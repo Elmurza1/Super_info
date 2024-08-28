8543d6e9443d3cb294976dce201224bb915684e5
elmyrza1 <elmyrza217@gmail.com>
2024-08-28 18:57:17 +0600

    settings

--- super_core/settings.py
+++ super_core/settings.py
@@ -131,10 +131,12 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'ru'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATICFILES_DIRS = [BASE_DIR / 'assets']
 
-STATIC_ROOT = 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
